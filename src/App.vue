8a62de6b0264625f23531f458e142e3cbba3f9dc
Senua <chc0089@gmail.com>
2023-03-25 18:53:13 +0100

    Vue Router Essentials

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
     
 
       <nav>
-        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/">Events</RouterLink> |
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
